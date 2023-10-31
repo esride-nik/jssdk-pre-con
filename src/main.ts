31574dd15d811c44928cd55612605bdab20cc5a3
Niklas Köhn <n.koehn@esri.de>
2023-10-31 08:55:29 +0100

    mapcomp init

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,17 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+// import { defineCustomElements as defineCalciteElements } from "@esri/calcite-components/dist/loader";
+// import { defineCustomElements as defineCodingElements } from "@arcgis/coding-components/dist/loader";
+import { defineCustomElements as defineMapElements } from "@arcgis/map-components/dist/loader";
+defineMapElements();
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// import "./style.css";
+// import typescriptLogo from "./typescript.svg";
+// import viteLogo from "/vite.svg";
+// import { setupCounter } from "./counter.ts";
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+//   <div>
+//     <arcgis-map item-id="05e015c5f0314db9a487a9b46cb37eca" />
+//   </div>
+// `;
+
+// setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
