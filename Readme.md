a16d627504b5db1fa1474949efb8ed39336b9cbe
David Hanimann <d.hanimann@esri.ch>
2023-11-02 13:45:46 +0100

    Add layer

--- Readme.md
+++ Readme.md
@@ -2,6 +2,7 @@
 
 ## Get Template to run
 1. Install VS Code
+    - Open VS Code and open a Terminal (Ctrl + Shift + P -> Toggle Terminal)
 2. Install git ```git -v```
 3. Install node ```node -v```
 4. Create new Vite Template `npm create vite@latest`
@@ -13,3 +14,4 @@
 2. Remove counter.ts, content of main.ts, style.css and images.
 3. Remove unneeded imports from html and add arcgis css
 4. Add css to style.css and add MapView in main.ts
+5. Add a Layer from Living Atlas
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import Map from '@arcgis/core/Map'
 import MapView from '@arcgis/core/views/MapView'
+import FeatureLayer from '@arcgis/core/layers/FeatureLayer'
+
+const layer = new FeatureLayer({ url: 'https://services2.arcgis.com/jUpNdisbWqRpMo35/arcgis/rest/services/Berliner_Mauer/FeatureServer/0' })
+const layer1 = new FeatureLayer({ url: 'https://services2.arcgis.com/jUpNdisbWqRpMo35/arcgis/rest/services/Berliner_Mauer/FeatureServer/1' })
+const layer2 = new FeatureLayer({ url: 'https://services2.arcgis.com/jUpNdisbWqRpMo35/arcgis/rest/services/Berliner_Mauer/FeatureServer/2' })
+const layer3 = new FeatureLayer({ url: 'https://services2.arcgis.com/jUpNdisbWqRpMo35/arcgis/rest/services/Berliner_Mauer/FeatureServer/3' })
 
 const map = new Map({
-  basemap: 'satellite'
+  basemap: 'satellite',
+  layers: [layer, layer1, layer2, layer3]
 })
 
 const mapView = new MapView({
@@ -11,3 +18,4 @@ const mapView = new MapView({
   zoom: 14,
   container: 'viewDiv'
 })
+
