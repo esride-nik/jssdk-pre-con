ec1f04a28830c50022b8589844821e6ad8360b27
Niklas Köhn <n.koehn@esri.de>
2023-11-03 11:43:42 +0100

    Step 1 - Add scene with basemap

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,24 @@
-import './style.css'
+import Map from "@arcgis/core/Map";
+import SceneView from "@arcgis/core/views/SceneView";
 
+
+/********************************************************************
+ * Step 1 - Add scene with basemap *
+ ********************************************************************/
+
+const map = new Map({
+  basemap: "dark-gray-vector",
+  ground: "world-elevation",
+});
+
+const view = new SceneView({
+  container: document.querySelector("#app") as HTMLDivElement,
+  center: [13.405, 52.52],
+  scale: 50000,
+  map: map,
+
+  environment: {
+    atmosphereEnabled: false,
+    starsEnabled: false,
+  },
+});
