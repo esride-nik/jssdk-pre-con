b98b071cd97f2f3a78ac65d7c056df9b5793f5c1
David Hanimann <d.hanimann@esri.ch>
2023-11-07 23:12:16 +0100

    Make arcgis types work

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,42 @@
 import Map from '@arcgis/core/Map'
+import SceneView from '@arcgis/core/views/SceneView'
 import MapView from '@arcgis/core/views/MapView'
 import FeatureLayer from '@arcgis/core/layers/FeatureLayer'
+import SceneLayer from '@arcgis/core/layers/SceneLayer'
 
 const layer = new FeatureLayer({ url: 'https://services2.arcgis.com/jUpNdisbWqRpMo35/arcgis/rest/services/Berliner_Mauer/FeatureServer/0' })
 const layer1 = new FeatureLayer({ url: 'https://services2.arcgis.com/jUpNdisbWqRpMo35/arcgis/rest/services/Berliner_Mauer/FeatureServer/1' })
 const layer2 = new FeatureLayer({ url: 'https://services2.arcgis.com/jUpNdisbWqRpMo35/arcgis/rest/services/Berliner_Mauer/FeatureServer/2' })
 const layer3 = new FeatureLayer({ url: 'https://services2.arcgis.com/jUpNdisbWqRpMo35/arcgis/rest/services/Berliner_Mauer/FeatureServer/3' })
 
+const scLayer = new SceneLayer({ url: 'https://tiles.arcgis.com/tiles/jUpNdisbWqRpMo35/arcgis/rest/services/Berlin_3D/SceneServer' })
+
 const map = new Map({
   basemap: 'satellite',
-  layers: [layer, layer1, layer2, layer3]
+  ground: 'world-elevation',
+  layers: [layer, layer1, layer2, layer3, scLayer]
 })
 
-const mapView = new MapView({
+// const mapView = new MapView({
+//   map: map,
+//   center: [13.4, 52.5],
+//   zoom: 14,
+//   container: 'viewDiv'
+// })
+
+const sceneView = new SceneView({
   map: map,
   center: [13.4, 52.5],
-  zoom: 14,
+  zoom: 5,
   container: 'viewDiv'
 })
+
+setTimeout(() => {
+
+  sceneView.goTo({
+    center: [13.392154, 52.512202],
+    tilt: 60,
+    zoom: 18
+  })
+}, 5000)
 
